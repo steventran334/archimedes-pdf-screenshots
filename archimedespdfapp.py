da70bdbde9043434e90814c6bf69a54ec18178ac
steventran334 <65040350+steventran334@users.noreply.github.com>
2025-05-22 16:03:31 -0400

    Update archimedespdfapp.py

--- archimedespdfapp.py
+++ archimedespdfapp.py
@@ -21,7 +21,7 @@ if uploaded_file is not None:
 
         # Final crop boxes
         graph_box = (120, 370, 1590, 1035)
-        table_box = (339, 1185, 1367, 1750)
+        table_box = (339, 1185, 1367, 1760)
 
         graph_img = img.crop(graph_box)
         table_img = img.crop(table_box)
